4074bd33828a0c88ccb825a708935f9f019a1fdc
Justin York <jyork@genward.com>
2021-03-02 17:46:41 -0700

    Setup an env that can help us find a solution

---
+++ test/Foo.test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Foo from 'src/Foo';
+
+it('test', () => {
+  render(<Foo />)
+});
--- test/react-query.test.js
+++ test/react-query.test.js
@@ -1,10 +1,10 @@
 import React from 'react';
-
 import {QueryClient, QueryClientProvider} from 'react-query';
 import { render } from '@testing-library/react';
+import Foo from 'src/Foo';
 
 const queryClient = new QueryClient();
 
 it('test', () => {
-  render(<QueryClientProvider client={queryClient}><div>foo</div></QueryClientProvider>)
+  render(<QueryClientProvider client={queryClient}><Foo /></QueryClientProvider>)
 });
